8dca1f3cc62b43d762392d3e63e6b4fd1e04b1ef
Bharmaglot <46847706+Bharmaglot@users.noreply.github.com>
2022-06-16 18:53:05 +0300

    slow_progress
    oh...

---
+++ Aster/Assets/Scripts/ConsoleInput.cs
@@ -0,0 +1,7 @@
+namespace Asteriods.Abstract_Factory
+{
+    internal sealed class ConsoleInput : IInput
+    {
+        public string Name => nameof(ConsoleInput);
+    }
+}
---
+++ Aster/Assets/Scripts/PCInput.cs
@@ -0,0 +1,7 @@
+namespace Asteriods.Abstract_Factory
+{
+    internal sealed class PCInput : IInput
+    {
+        public string Name => nameof(PCInput);
+    }
+}
